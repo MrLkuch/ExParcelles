c09cd16175b0cdc1323b434066889317861e396b
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-07-18 10:08:02 +0200

    feat(Unite.cs): create class unite

---
+++ ExParcelles/Classes/Unite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExParcelles.Classes
+{
+    internal class Unite
+    {
+        private int _val_unite;
+
+        public int Val_unite { get => _val_unite; set => _val_unite = value; }
+
+        public Unite(int val_unite)
+        {
+            Val_unite = val_unite;
+        }
+    }
+}
