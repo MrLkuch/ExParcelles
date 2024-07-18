c509a4b66a02f7e338bf3d8a76a418d21b994833
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-07-18 10:10:51 +0200

    fix(Unite.cs): fix data type

--- ExParcelles/Classes/Unite.cs
+++ ExParcelles/Classes/Unite.cs
@@ -8,11 +8,11 @@ namespace ExParcelles.Classes
 {
     internal class Unite
     {
-        private int _val_unite;
+        private String _val_unite;
 
-        public int Val_unite { get => _val_unite; set => _val_unite = value; }
+        public String Val_unite { get => _val_unite; set => _val_unite = value; }
 
-        public Unite(int val_unite)
+        public Unite(String val_unite)
         {
             Val_unite = val_unite;
         }
