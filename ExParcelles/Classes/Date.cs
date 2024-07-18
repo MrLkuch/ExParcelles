f2705dcd7d56d683e7bb77ca16e0c2b2a4282763
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-07-18 10:11:18 +0200

    feat(Date.cs): create class date

---
+++ ExParcelles/Classes/Date.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExParcelles.Classes
+{
+    internal class Date
+    {
+        private DateTime _val_date;
+
+        public DateTime Val_date { get => _val_date; set => _val_date = value; }
+
+        public Date(DateTime val_date)
+        {
+            Val_date = val_date;
+        }
+    }
+}
