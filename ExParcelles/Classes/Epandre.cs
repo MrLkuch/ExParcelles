0c07ac9c869b09ae22e936469f53bbddfc136c8b
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-07-18 14:26:31 +0200

    feat(Epandre.cs): create epandre class

---
+++ ExParcelles/Classes/Epandre.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExParcelles.Classes
+{
+    internal class Epandre
+    {
+        private int _qte_epandue;
+
+        private int _id_engrais;
+        private int _id_parcelle;
+        private DateTime _date;
+
+        public int Qte_epandue { get => _qte_epandue; set => _qte_epandue = value; }
+        
+        public int Id_engrais { get => _id_engrais;}
+        public int Id_parcelle { get => _id_parcelle;}
+        public DateTime Date { get => _date; }
+
+        public Epandre(int qte_epandue, Engrais engrais, Parcelle parcelle, Date date)
+        {
+            Qte_epandue = qte_epandue;
+
+            _id_engrais = engrais.Id_engrais;
+            _id_parcelle = parcelle.Id_parcelle;
+            _date = date.Val_date;
+            
+        }
+    }
+}
